cc70c464215f43aa7e6a258a822a191046a6e30f
valberandrade <valber.andrade@hotmail.com>
2024-07-16 19:49:31 -0300

    Atualizando projeto

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+//        User::factory()->create([
+//            'name' => 'Valber Andrade',
+//            'username' => 'valberandrade',
+//            'email' => 'valber@maro.cc',
+//            'role' => 'admin',
+//            'status' => 'active',
+//            'password' => bcrypt('password')
+//        ]);
+//        User::factory()->create([
+//            'name' => 'Vendedor Vendor',
+//            'username' => 'vendor',
+//            'email' => 'vendor@email.com',
+//            'role' => 'vendor',
+//            'status' => 'active',
+//            'password' => bcrypt('password')
+//        ]);
+//        User::factory()->create([
+//            'name' => 'Cliente User',
+//            'username' => 'user',
+//            'email' => 'cliente@email.com',
+//            'role' => 'user',
+//            'status' => 'active',
+//            'password' => bcrypt('password')
+//        ]);
+    }
+}
